abc6db539aca04b8e29f4916f70e8d76aef9b196
Théo DELECOUR <theo.delecour@gmx.com>
2019-07-29 13:28:19 +0200

    Divide graph creation in 2 functions

--- corregraphe/core.py
+++ corregraphe/core.py
@@ -1,7 +1,8 @@
+from typing import Dict
+
 import hvplot.networkx as hvnx
 import networkx as nx
 from pandas import DataFrame
-from typing import Dict, Tuple
 
 
 def CorrelationGraph(object):
@@ -21,13 +22,14 @@ def CorrelationGraph(object):
         self._data = data
         self._method = method
         self.correlations = self._compute_correlations(self._data, self._method)
-        self.graph, self.pos = self._create_graph(self.correlations)
+        self.graph = self._create_graph(self.correlations)
+        self.pos = self._compute_positions(self.graph)
 
     def draw(self, **kwargs):
         return hvnx.draw(
             self.graph,
             pos=self.pos,
-            edge_width="weight",
+            edge_width="correlation",
             node_color="weight",
             labels="name",
             colorbar=True,
@@ -36,26 +38,31 @@ def CorrelationGraph(object):
 
     @staticmethod
     def _compute_correlations(data: DataFrame, method: str) -> DataFrame:
+        """Computes correlation between columns of dataframe."""
         return data.corr(method=method).abs()
 
     @staticmethod
-    def _create_graph(correlations: DataFrame) -> Tuple[nx.Graph, Dict]:
+    def _create_graph(correlations: DataFrame) -> nx.Graph:
+        """Creates a graph object to represent correlations."""
         graph = nx.complete_graph(correlations.shape[1])
         graph = nx.relabel_nodes(
             graph, {i: col for i, col in enumerate(correlations.columns)}
         )
 
         for edge in graph.edges:
-            graph.edges[edge]["weight"] = correlations[edge[0]][edge[1]]
+            graph.edges[edge]["correlation"] = correlations[edge[0]][edge[1]]
 
         for node in graph.nodes:
             graph.nodes[node]["name"] = node
 
-        pos = nx.spring_layout(graph)
-
-        for node, coef in nx.clustering(graph, weight="weight").items():
+        for node, coef in nx.clustering(graph, weight="correlation").items():
             graph.nodes[node]["weight"] = coef
-        return graph, pos
+
+        return graph
+
+    @staticmethod
+    def _compute_positions(graph: nx.Graph) -> Dict:
+        return nx.spring_layout(graph)
 
     def __repr__(self) -> str:
-        pass
+        return self.__class__.__name__
