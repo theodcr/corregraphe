4bf6248631185d9cd4b68699a7391c93850c9a4d
Théo DELECOUR <theo.delecour@gmx.com>
2019-07-29 12:49:29 +0200

    Start class implementation

--- corregraphe/__init__.py
+++ corregraphe/__init__.py
@@ -1,1 +1,27 @@
 __version__ = "0.1dev"
+
+
+def CorrelationGraph(object):
+    """
+    Creates a correlation graph from dataframe.
+
+    Parameters
+    ----------
+    """
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def _check_inputs(self):
+        pass
+
+    def _correlation(self):
+        pass
+
+    def _create_graph(self):
+        pass
+
+    def __repr__(self) -> str:
+        pass
