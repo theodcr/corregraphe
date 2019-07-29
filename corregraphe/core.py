192bc771b9d92321f8f0f40a92bf264c29282b97
Théo DELECOUR <theo.delecour@gmx.com>
2019-07-29 13:13:20 +0200

    Add methods to create graph

--- corregraphe/core.py
+++ corregraphe/core.py
@@ -1,3 +1,4 @@
+import networkx as nx
 import pandas as pd
 
 
@@ -13,9 +14,12 @@ def CorrelationGraph(object):
     method : str = 'spearman' {'pearson', 'kendall', 'spearman'}
         correlation method, see pandas.DataFrame.corr
     """
+
     def __init__(self, data: pd.DataFrame, method: str = "spearman"):
         self._data = data
         self._method = method
+        self.correlations = self._compute_correlations(self._data, self._method)
+        self.graph, self.pos = self._create_graph(self.correlations)
 
     def draw(self):
         pass
@@ -23,11 +27,28 @@ def CorrelationGraph(object):
     def _check_inputs(self):
         pass
 
-    def _correlation(self):
-        pass
+    @staticmethod
+    def _compute_correlations(data: pd.DataFrame, method: str):
+        return data.corr(method=method).abs()
 
-    def _create_graph(self):
-        pass
+    @staticmethod
+    def _create_graph(correlations: pd.DataFrame):
+        graph = nx.complete_graph(correlations.shape[1])
+        graph = nx.relabel_nodes(
+            graph, {i: col for i, col in enumerate(correlations.columns)}
+        )
+
+        for edge in graph.edges:
+            graph.edges[edge]["weight"] = correlations[edge[0]][edge[1]]
+
+        for node in graph.nodes:
+            graph.nodes[node]["name"] = node
+
+        pos = nx.spring_layout(graph)
+
+        for node, coef in nx.clustering(graph, weight="weight").items():
+            graph.nodes[node]["weight"] = coef
+        return graph, pos
 
     def __repr__(self) -> str:
         pass
